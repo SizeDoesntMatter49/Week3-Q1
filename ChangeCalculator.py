39a5b643bb1f216ee9dfcd42da63bc82c4781737
John S Thanyanop <156885586+SThanyanop@users.noreply.github.com>
2025-08-27 21:14:07 +0700

    Add files via upload

---
+++ ChangeCalculator.py
@@ -0,0 +1,14 @@
+'''
+Help store by creating Python function compute the change using the least numbers of
+coins/bills.
+Assume that the values are in full baht only and the possible coins/bills are 1, 5, 10, 20, 50, 100,
+500, 1000 baht per coin/bill.
+'''
+
+# Student do quiz here below
+# do not change the function name
+# do not change the function input
+
+def ChangeCalculator(price,paid):
+
+    return [0]*8
